07fd9860cc224c389bc91d2175b7bc46fd917740
Derrick Munene Kariuki <derrickmunene2006@gmail.com>
2025-12-18 20:30:34 +0300

    added ohm's calculator

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function buttonClicked() {
     document.getElementById("mainDiv").textContent =
         "Button Clicked times: " + numButtonClicks;
 }
+
+function calculateOhm() {
+    const v = document.getElementById('voltage').value;
+    const i = document.getElementById('current').value;
+    const resultDisplay = document.getElementById('result');
+
+
+    if (v > 0 && i > 0) {
+        const r = v / i;
+        resultDisplay.innerHTML = `Result: <strong>${r.toFixed(2)} Ω (Ohms)</strong>`;
+        resultDisplay.style.color = "#2ecc71"; // Green for success
+    } else {
+        resultDisplay.innerHTML = "Please enter valid Voltage and Current!";
+        resultDisplay.style.color = "#e74c3c"; // Red for error
+    }
+}
